d502dce8f7af08830ca36f37cc593d19dedccaf4
root <root@ip-10-21-0-232.ap-south-1.compute.internal>
2025-08-18 06:01:20 +0000

    removed jsonify

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/health", methods=["GET"])
 def health():
     return "200 OK", 200
 
+
 @app.route("/data", methods=["GET"])
 def data():
     return "Hello from DevOps!", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
